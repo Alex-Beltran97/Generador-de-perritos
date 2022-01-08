ad06f6add2a38bdd58e6c018ef3829151f80814c
Alex-Beltran97 <92172762+Alex-Beltran97@users.noreply.github.com>
2022-01-08 14:07:11 -0500

    Generador de perritos version funcional N.1

---
+++ app.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".btn");
+
+const API = async ()=>{
+    const laAPI = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await laAPI.json();
+    
+    document.querySelector(".load").setAttribute("style","display: in block")
+    
+    setTimeout(()=>{
+        document.getElementById("imagen").setAttribute("src",data.message);
+        console.log(data.status);
+        document.querySelector(".load").setAttribute("style","display: none")
+    },2000)
+
+}
+
+button.addEventListener("click",API)
